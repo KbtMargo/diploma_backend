b8998f83ac9a6228a347d31d9734b3d72ef279dc
KbtMargo <kubatiycom2@gmail.com>
2026-03-19 21:29:00 +0200

    creating structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JobsModule } from './jobs/jobs.module';
+import { ApplicationsModule } from './applications/applications.module';
+import { CompaniesModule } from './companies/companies.module';
+import { SkillsModule } from './skills/skills.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AnalyticsModule } from './analytics/analytics.module';
+import { AdminModule } from './admin/admin.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, JobsModule, ApplicationsModule, CompaniesModule, SkillsModule, NotificationsModule, AnalyticsModule, AdminModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
